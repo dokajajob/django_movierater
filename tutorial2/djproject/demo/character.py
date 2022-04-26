eb6ddd58ba7229e649dc09f10cc2238990e77677
dokajajob <75301203+dokajajob@users.noreply.github.com>
2022-04-26 13:25:40 +0300

    Delete tutorial2 directory

--- tutorial2/djproject/demo/character.py
+++
@@ -1,7 +0,0 @@
-# from djproject.demo import models
-# from .models import Items
-#
-#
-# class Character(models.Model):
-#     name = models.CharField(max_length=30)
-#     items = models.ForeignKey(Items, on_delete=models.CASCADE, related_name='characters')
